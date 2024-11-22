07c24814af4233e89de0c8c7498ef4b74a470b96
Dominik Bernroider <dominik.bernroider@icloud.com>
2024-11-22 02:16:35 +0100

    Update .nvim.lua (Last time i swear)

--- .nvim.lua
+++ .nvim.lua
@@ -1,14 +1,9 @@
-vim.api.nvim_create_autocmd("VimEnter", {
-	group = vim.api.nvim_create_augroup("riverpod_code_generator", { clear = true }),
-	callback = function()
-		vim.system({ "./.fvm/flutter_sdk/bin/dart", "run", "build_runner", "watch" }, { text = true }, function(out)
-			if out.code ~= 0 then
-				print("Error while starting code generator:")
-				print(out.stdout)
-			end
-		end)
-	end,
-})
+vim.system({ "./.fvm/flutter_sdk/bin/dart", "run", "build_runner", "watch" }, { text = true }, function(out)
+	if out.code ~= 0 then
+		print("Error while starting code generator:")
+		print(out.stdout)
+	end
+end)
 
 table.insert(require("nvim-tree").config.filters.custom, "**/*\\.g\\.dart")
 table.insert(require("nvim-tree").config.filters.custom, "**/*\\.freezed\\.dart")
